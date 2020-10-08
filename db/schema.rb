c9cc49969ba35bc73536bbef0c1890823f1b96b4
Juan <juan.jimenez@uc.cl>
2020-10-08 18:03:51 -0300

    Revert "Changed landing page routes"
    This reverts commit 696cbce37d009bff58f533ac343a26127dfe3576.

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_160418) do
     t.text "description"
     t.integer "capacity"
     t.integer "price"
-    t.float "rating"
+    t.float "rating", default: 5.0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
