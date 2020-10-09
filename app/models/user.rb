4eff55696c360bda436d53956320d201bea68d39
victoria <victoria@victorias-MacBook-Pro.local>
2020-10-09 18:27:39 -0300

    edit listo

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :parties, dependent: :destroy
   has_many :services, dependent: :destroy
+  has_many :scomments
   validates :name, presence: true
   validates :age, presence: true
   validates :phone, presence: true
