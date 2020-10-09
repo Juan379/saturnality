910fc0b3dd8841d62f15ff4fa8441fae4176edcb
Juan <juan.jimenez@uc.cl>
2020-10-09 14:49:59 -0300

    changed format user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,45 +2,47 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'creation' do
-    it 'can be created' do
+    it 'should be created correctly' do
       user = User.create(email: "testuser@gmail.com", name:"test", age: 18, phone:123456789, password: "asdffgg", password_confirmation: "asdffgg")
-      
       expect(user).to be_valid
     end
-    it 'can be created and save email' do
+
+    it 'should be created and save email' do
       user = User.create(email: "testuser@gmail.com", name:"test", age: 18, phone:123456789, password: "asdffgg", password_confirmation: "asdffgg")
-      
       expect(user.email).to eq("testuser@gmail.com")
     end
-    it 'can be created and save name' do
+    
+    it 'should be created and save name' do
       user = User.create(email: "testuser@gmail.com", name:"test", age: 18, phone:123456789, password: "asdffgg", password_confirmation: "asdffgg")
-      
       expect(user.name).to eq("test")
     end
-    it 'can be created and save age' do
+
+    it 'should be created and save age' do
       user = User.create(email: "testuser@gmail.com", name:"test", age: 18, phone:123456789, password: "asdffgg", password_confirmation: "asdffgg")
-      
       expect(user.age).to eq(18)
     end
-    it 'can be created and save phone' do
+   
+    it 'should be created and save phone' do
       user = User.create(email: "testuser@gmail.com", name:"test", age: 18, phone:"123456789", password: "asdffgg", password_confirmation: "asdffgg")
-      
       expect(user.phone).to eq("123456789")
     end
-    it 'cant be created without name' do
-      user = User.create(email: "testuser@gmail.com", age: 18, phone:"123456789", password: "asdffgg", password_confirmation: "asdffgg")
+    
+    context "when invalid inputs are given to create a User" do
+
+      it "shouldn't be created without name" do
+        user = User.create(email: "testuser@gmail.com", age: 18, phone:"123456789", password: "asdffgg", password_confirmation: "asdffgg")
+        expect(user).not_to be_valid
+      end
+
+      it "shouldn't be created without phone" do
+        user = User.create(email: "testuser@gmail.com", age: 18, name:"test", password: "asdffgg", password_confirmation: "asdffgg")
+        expect(user).not_to be_valid
+      end
       
-      expect(user).not_to be_valid
-    end
-    it 'cant be created without phone' do
-      user = User.create(email: "testuser@gmail.com", age: 18, name:"test", password: "asdffgg", password_confirmation: "asdffgg")
-      
-      expect(user).not_to be_valid
-    end
-    it 'cant be created without age' do
-      user = User.create(email: "testuser@gmail.com", phone: "18", name:"test", password: "asdffgg", password_confirmation: "asdffgg")
-      
-      expect(user).not_to be_valid
+      it "shouldn't be created without age" do
+        user = User.create(email: "testuser@gmail.com", phone: "18", name:"test", password: "asdffgg", password_confirmation: "asdffgg")
+        expect(user).not_to be_valid
+      end
     end
   end
 end
