8c65223c7dbcedb0a1546917beeaa841becaf2a3
Juan <juan.jimenez@uc.cl>
2020-11-01 21:28:31 -0300

    rubucop fixes

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -3,23 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe Contract, type: :model do
-
-context 'When a service and valid parties are recieved' do
+  context 'When a service and valid parties are recieved' do
     it 'is created correctly' do
-        user1 = User.new(email: 'testuser@gmail.com', name: 'test', age: 18, phone: 123_456_789,
-                        password: 'asdffgg', password_confirmation: 'asdffgg')
-        user2 = User.new(email: 'testuser@gmail.com', name: 'test', age: 18, phone: 123_456_789,
-                        password: 'asdffgg', password_confirmation: 'asdffgg')
-        comuna = Comuna.new(name: 'San Antonio')
-        party = Party.new(description: 'This is the best party ever',
-                                    address: 'Roble drive 123', capacity: 23, cost: 25_000,
-                                    search: true, user: user1, comuna: comuna)
-        service = Service.new(name: 'CleaningService',
-                                    description: 'The best cleaning service available',
-                                    capacity: 20, price: 50_000, rating: 3.0, user: user2)
-        contract = Contract.new(service: service, party: party)
-        expect(contract).to be_valid
+      user1 = User.new(email: 'testuser@gmail.com', name: 'test', age: 18, phone: 123_456_789,
+                       password: 'asdffgg', password_confirmation: 'asdffgg')
+      user2 = User.new(email: 'testuser@gmail.com', name: 'test', age: 18, phone: 123_456_789,
+                       password: 'asdffgg', password_confirmation: 'asdffgg')
+      comuna = Comuna.new(name: 'San Antonio')
+      party = Party.new(description: 'This is the best party ever',
+                        address: 'Roble drive 123', capacity: 23, cost: 25_000,
+                        search: true, user: user1, comuna: comuna)
+      service = Service.new(name: 'CleaningService',
+                            description: 'The best cleaning service available',
+                            capacity: 20, price: 50_000, rating: 3.0, user: user2)
+      contract = described_class.new(service: service, party: party)
+      expect(contract).to be_valid
     end
-    end
-    
+  end
 end
