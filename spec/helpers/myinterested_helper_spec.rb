d9232217f3eb1e99430c24b3cccfa76147ff79f9
Ami Ustilovsky <austilovsky@uc.cl>
2020-11-22 11:32:09 -0300

    avance

--- spec/helpers/myinterested_helper_spec.rb
+++ spec/helpers/myinterested_helper_spec.rb
@@ -10,6 +10,43 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe MyinterestedHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe PartiesHelper, type: :helper do
+  
+  describe "#metodo2" do
+    it "returns true" do
+      user = User.new(email: 'testuser@gmail.com', name: 'test', age: 18,
+        phone: 123_456_789, password: 'asdffgg', password_confirmation: 'asdffgg')
+comuna = Comuna.new(name: 'San Antonio')
+party = Party.create(title: 'The best', description: 'This is the best party ever',
+             address: 'Roble drive 123', capacity: 23, cost: 25_000,
+             search: true, user: user, comuna: comuna)
+      expect(helper.how_much_interesteds(party)).to equal(0)
+    end
+end
+
+describe "#metodo3" do
+  it "returns true" do
+    user = User.new(email: 'testuser@gmail.com', name: 'test', age: 18,
+      phone: 123_456_789, password: 'asdffgg', password_confirmation: 'asdffgg')
+comuna = Comuna.new(name: 'San Antonio')
+party = Party.create(title: 'The best', description: 'This is the best party ever',
+           address: 'Roble drive 123', capacity: 23, cost: 25_000,
+           search: true, user: user, comuna: comuna)
+    expect(helper.how_much_money(party)).to equal(0)
+  end
+end
+
+describe "#metodo1" do
+  it "returns true" do
+    user = User.new(email: 'testuser@gmail.com', name: 'test', age: 18,
+      phone: 123_456_789, password: 'asdffgg', password_confirmation: 'asdffgg')
+comuna = Comuna.new(name: 'San Antonio')
+party = Party.create(title: 'The best', description: 'This is the best party ever',
+           address: 'Roble drive 123', capacity: 23, cost: 25_000,
+           search: true, user: user, comuna: comuna)
+    expect(helper.user_is_interested(user, party)).to be(false)
+  end
+end
+
+
 end
