5aa7db51781441b267e3975608ab2feaebb4417a
Juan <juan.jimenez@uc.cl>
2020-11-22 18:54:09 -0300

    pending and accepted service views ready

--- app/controllers/mypservices_controller.rb
+++ app/controllers/mypservices_controller.rb
@@ -1,6 +1,6 @@
 class MypservicesController < ApplicationController
     def show
         @user = current_user
-        @pending_services = Service.where(user_id: @user.id, status: "pending")
+        @services = Service.where(user_id: @user.id, status: "pending")
     end
 end
