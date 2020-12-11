7b8119469dd024736579f7149d5c05f9e51cab01
victoria <victoriasb@uc.cl>
2020-12-11 17:54:53 -0300

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,20 +16,15 @@ admin = User.create(email: "admin@gmail.com", name: "admin", age: 21, phone: "12
 admin.add_role :admin
 
 
-u1 = User.create(email: "juan@gmail.com", name: "Juan", age: 21, phone: "123123", password:"qwerty")
-u2 = User.create(email: "ami@gmail.com", name: "Ami", age: 21, phone: "123123", password:"qwerty")
-u3 = User.create(email: "vicky@gmail.com", name: "Vicky", age: 21, phone: "123123", password:"qwerty")
-u4 =User.create(email: "jose@gmail.com", name: "Jose", age: 21, phone: "123123", password:"qwerty")
-u5 =User.create(email: "nico@gmail.com", name: "Nico", age: 21, phone: "123123", password:"qwerty")
-u6 =User.create(email: "sofia@gmail.com", name: "Sofia", age: 21, phone: "123123", password:"qwerty")
-u7 =User.create(email: "clemente@gmail.com", name: "Clemente", age: 21, phone: "123123", password:"qwerty")
+u1 = User.create(email: "juan@gmail.com", name: "Juan", age: 21, phone: "923123", password:"qwerty")
+u2 = User.create(email: "ami@gmail.com", name: "Ami", age: 21, phone: "923123", password:"qwerty")
+u3 = User.create(email: "vicky@gmail.com", name: "Vicky", age: 21, phone: "923123", password:"qwerty")
+u4 =User.create(email: "jose@gmail.com", name: "Jose", age: 21, phone: "923123", password:"qwerty")
+u5 =User.create(email: "nico@gmail.com", name: "Nico", age: 21, phone: "923123", password:"qwerty")
+u6 =User.create(email: "sofia@gmail.com", name: "Sofia", age: 21, phone: "923123", password:"qwerty")
+u7 =User.create(email: "clemente@gmail.com", name: "Clemente", age: 21, phone: "923123", password:"qwerty")
 
 
-Party.create(title: "JuanP1", description: "Juan Party n1", address: "123123", capacity: 123, cost: 12500, comuna: c1, user: u1)
-Party.create(title: "JmiP1", description: "Juan Party n2", address: "123123", capacity: 123, cost: 12500, comuna: c6, user: u1)
-Party.create(title: "AmiP1", description: "Ami Party n1", address: "123123", capacity: 123, cost: 12500, comuna: c2, user: u2)
-Party.create(title: "AmiP2", description: "Ami Party n2", address: "123123", capacity: 123, cost: 12500, comuna: c4, user: u2)
-Party.create(title: "VickyP1", description: "Vicky Party n1", address: "123123", capacity: 123, cost: 12500, comuna: c4, user: u3)
-Party.create(title: "VickyP2", description: "Vicky Party n2", address: "123123", capacity: 123, cost: 12500, comuna: c5, user: u3)
-Party.create(title: "JoseP1", description: "Jose Party n1", address: "123123", capacity: 123, cost: 12500, comuna: c1, user: u4)
-Party.create(title: "JoseP2", description: "Jose Party n2", address: "123123", capacity: 123, cost: 12500, comuna: c3, user: u4)
+Party.create(title: "Costume Party", description: "halloween party 2.0", address: "123123", capacity: 123, cost: 12500, comuna: c1, user: u1)
+Party.create(title: "Beerpong championship", description: "winner gets 3 6packs", address: "123123", capacity: 123, cost: 12500, comuna: c6, user: u1)
+Party.create(title: "shrek themed party", description: "come dressed as your favorite shrek character", address: "123123", capacity: 123, cost: 12500, comuna: c2, user: u2)
